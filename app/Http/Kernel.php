7d928b218988603c1c88f6b7c7a80f7971633093
caoquan95 <aquancva@gmail.com>
2018-06-05 14:40:18 +0700

    add cors middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            'cors'
         ],
     ];
 
@@ -59,6 +60,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'getSubDomain' => \App\Http\Middleware\GetSubDomain::class
+        'getSubDomain' => \App\Http\Middleware\GetSubDomain::class,
+        'cors' => \App\Http\Middleware\Cors::class
     ];
 }

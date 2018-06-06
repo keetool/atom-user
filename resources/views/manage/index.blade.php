5fe8075464bdb2bbc8d5c228ddfb8838e301e7b7
phanmduong <phanminhduong@hotmail.com>
2018-06-06 16:52:23 +0700

    EDIT build manage

--- resources/views/manage/index.blade.php
+++ resources/views/manage/index.blade.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <title>Title</title>
     <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-    <link href="https://d255zuevr6tr8p.cloudfront.net/build-manage/vendors~main.302399239038d62de217.chunk.css" rel="stylesheet">
-    <link href="https://d255zuevr6tr8p.cloudfront.net/build-manage/main.aa81807946327fe81e4b.css" rel="stylesheet">
+    <link href="https://d255zuevr6tr8p.cloudfront.net/build-manage/vendors~main.2292c743f73598ff731e.chunk.css" rel="stylesheet">
+    <link href="https://d255zuevr6tr8p.cloudfront.net/build-manage/main.f1425aacf4af530a6a1f.css" rel="stylesheet">
 </head>
 
 <body>
     <div id="app"></div>
-    <script type="text/javascript" src="https://d255zuevr6tr8p.cloudfront.net/build-manage/vendors~main.ff2d3d7dc07ef1a403b7.chunk.js"></script>
-    <script type="text/javascript" src="https://d255zuevr6tr8p.cloudfront.net/build-manage/main.ff2d3d7dc07ef1a403b7.js"></script>
+    <script type="text/javascript" src="https://d255zuevr6tr8p.cloudfront.net/build-manage/vendors~main.c1a0a6acecc40bb43631.chunk.js"></script>
+    <script type="text/javascript" src="https://d255zuevr6tr8p.cloudfront.net/build-manage/main.c1a0a6acecc40bb43631.js"></script>
 </body>
 
 </html>

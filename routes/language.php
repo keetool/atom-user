eccde1c396bc2d847ead56fc81f46ec5b75b0611
caoquan95 <aquancva@gmail.com>
2018-06-06 11:44:53 +0700

    add language unit test

--- routes/language.php
+++ routes/language.php
@@ -8,8 +8,8 @@ Route::prefix("language")->group(function() {
     Route::get('add', 'LanguageController@getAddLanguage');
     Route::post('/', 'LanguageController@postAddLanguage');
     Route::get("/{id}", 'LanguageController@getLanguageDetail');
-    Route::get('/{id}/edit','LanguageController@getEditLanguage');
 
+    Route::get('/{id}/edit','LanguageController@getEditLanguage');
     Route::get("/{lang_id}/keyword","LanguageController@getKeywordAdd");
     Route::get("/{lang_id}/keyword/add","LanguageController@getKeywordAdd");
     Route::get("/{lang_id}/keyword/{keyword_id}","LanguageController@getKeywordEdit");

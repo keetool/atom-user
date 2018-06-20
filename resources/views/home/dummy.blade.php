8c6b4a30551b82f57a8a5b0178030efd6488b114
datvithanh98 <datvithanh98@gmail.com>
2018-06-20 15:24:10 +0700

    fb login test2

--- resources/views/home/dummy.blade.php
+++ resources/views/home/dummy.blade.php
@@ -1,49 +1,54 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>
 
-        </title>
-    </head>
-    <body>
+<head>
+    <title>
+
+    </title>
+</head>
+
+<body>
     <script>
-        window.fbAsyncInit = function() {
-            FB.init({
-            appId      : '2003668823296017',
-            cookie     : true,
-            xfbml      : true,
-            version    : 'v3.0'
-            });
-            
-            FB.AppEvents.logPageView();   
-            
-        };
+        $(document).ready(function () {
+            window.fbAsyncInit = function () {
+                FB.init({
+                    appId: '2003668823296017',
+                    cookie: true,
+                    xfbml: true,
+                    version: 'v3.0'
+                });
 
-        (function(d, s, id){
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {return;}
-            js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
+                FB.AppEvents.logPageView();
 
+            };
 
-        FB.getLoginStatus(function(response) {
+            (function (d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {
+                    return;
+                }
+                js = d.createElement(s);
+                js.id = id;
+                js.src = "https://connect.facebook.net/en_US/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        });
+
+        FB.getLoginStatus(function (response) {
             statusChangeCallback(response);
         });
 
         function checkLoginState() {
-            FB.getLoginStatus(function(response) {
+            FB.getLoginStatus(function (response) {
                 statusChangeCallback(response);
             });
         }
+
     </script>
 
-        <fb:login-button 
-        scope="public_profile,email"
-        onlogin="checkLoginState();">
-        </fb:login-button>
-        asdasdk
-    </body>
-    
+    <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+    </fb:login-button>
+    asdasdk
+</body>
+
 </html>

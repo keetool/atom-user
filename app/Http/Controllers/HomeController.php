42a3c96fe453a11f4f3e0528f40f24b5154d846f
datvithanh98 <datvithanh98@gmail.com>
2018-06-22 09:58:48 +0700

    google login test4

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view("home.index");
+        return view("home.dummy");
     }
 
     public function register()

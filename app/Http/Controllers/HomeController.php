8816b4642005c009987c032fc1e4f6233f490e16
datvithanh98 <datvithanh98@gmail.com>
2018-06-23 10:33:38 +0700

    docs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\File;
 
 class HomeController extends Controller
 {

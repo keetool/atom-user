d093dc8e59c0608707c09c937ab7519e499f1b15
Cao Anh Quan <aquancva@gmail.com>
2018-06-23 10:46:09 +0700

    add log posts

--- app/Logs/Post/PostLog.php
+++ app/Logs/Post/PostLog.php
@@ -1,11 +1,10 @@
 <?php
-namespace App\Logs;
+namespace App\Logs\Post;
 
 use App\Logs\Log;
 use App\Objects\PostJsonObject;
 use App\Objects\UserJsonObject;
 use App\Objects\KeyJsonObject;
-use App\Objects\MerchantJsonObject;
 
 
 class PostLog extends Log
@@ -18,16 +17,11 @@ class PostLog extends Log
         $this->post = $post;
     }
 
-    /**log
-     * [
-     *  {type: "key", data: "manage.log.merchant.create"}
-     * ]
-     */
     protected function format()
     {
         return json_encode([
             (new UserJsonObject($this->user))->toArray(),
-            (new KeyJsonObject('manage.log.post.create'))->toArray(),
+            (new KeyJsonObject($this->action))->toArray(),
             (new PostJsonObject($this->post))->toArray()
         ]);
     }

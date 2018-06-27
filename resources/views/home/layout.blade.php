1de1a711b6a15b084828a7c1d5cdd9baeef89b79
Nora <minhnora98@gmail.com>
2018-06-27 13:31:10 +0700

    ADD Google Tags

--- resources/views/home/layout.blade.php
+++ resources/views/home/layout.blade.php
@@ -26,8 +26,22 @@
     <!--     Fonts and icons     -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
     <link rel="icon" href="https://d1j8r0kxyu9tj8.cloudfront.net/files/1529571679WLoLVlC4nzjl0yd.png">
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({‘gtm.start’:
+        new Date().getTime(),event:‘gtm.js’});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!=‘dataLayer’?‘&l=‘+l:‘’;j.async=true;j.src=
+        ’https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,‘script’,‘dataLayer’,‘GTM-M6L5HXR’);
+    </script>
+    <!-- End Google Tag Manager -->
 </head>
 <body class="overflow-hidden">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M6L5HXR" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <header id="home">
 
         <!-- navbar -->

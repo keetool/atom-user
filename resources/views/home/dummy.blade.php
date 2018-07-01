847efd68a59385d8511bf3e42337ab91d093478d
Cao Anh Quan <aquancva@gmail.com>
2018-07-01 16:55:35 +0700

    up

--- resources/views/home/dummy.blade.php
+++ resources/views/home/dummy.blade.php
@@ -8,8 +8,8 @@
 {{--<div class="g-signin2" data-onsuccess="onSignIn"></div>--}}
 <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.1.1/socket.io.js"></script>
     <script>
-        var socket = io('http://localhost:9000');
-        // var socket = io('https://atomuser.com:9000');
+        // var socket = io('http://localhost:9000');
+        var socket = io('https://atomuser.com:9000');
         socket.on("channel:event", function (data) {
             console.log(data);
         });

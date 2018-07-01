a9ddeaca5bfb94326766815eb388658969b59679
phanmduong <phanminhduong@hotmail.com>
2018-07-01 16:24:10 +0700

    UPDATE build social

--- resources/views/social/index.blade.php
+++ resources/views/social/index.blade.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <title>Title</title>
     <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-    <link href="https://d255zuevr6tr8p.cloudfront.net/build-social/vendors~main.42a589d0c78ed474debf.chunk.css"
+    <link href="https://d255zuevr6tr8p.cloudfront.net/build-social/vendors~main.39e2843c555ce5b43b1c.chunk.css"
           rel="stylesheet">
-    <link href="https://d255zuevr6tr8p.cloudfront.net/build-social/main.b2c096d11a37006c63c6.css" rel="stylesheet">
+    <link href="https://d255zuevr6tr8p.cloudfront.net/build-social/main.1f13a4f31f46c3caa547.css" rel="stylesheet">
 </head>
 <body>
 <div id="fb-root"></div>
@@ -24,14 +24,13 @@
     });
 </script>
 <script>!function (e, t, n) {
-        var o, s = e.getElementsByTagName(t)[0];
-        e.getElementById(n) || ((o = e.createElement(t)).id = n, o.src = "https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.0&appId=2003668823296017&autoLogAppEvents=1", s.parentNode.insertBefore(o, s))
-    }(document, "script", "facebook-jssdk")</script>
+    var o, s = e.getElementsByTagName(t)[0];
+    e.getElementById(n) || ((o = e.createElement(t)).id = n, o.src = "https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.0&appId=2003668823296017&autoLogAppEvents=1", s.parentNode.insertBefore(o, s));
+}(document, "script", "facebook-jssdk");</script>
 <div id="app"></div>
 <script type="text/javascript"
-        src="https://d255zuevr6tr8p.cloudfront.net/build-social/vendors~main.1e153960d2ad1cd35db7.chunk.js"></script>
-<script type="text/javascript" src="https://d255zuevr6tr8p.cloudfront.net/build-social/main.1e153960d2ad1cd35db7.js">
-
-</script>
+        src="https://d255zuevr6tr8p.cloudfront.net/build-social/vendors~main.5af497e541163275bb82.chunk.js"></script>
+<script type="text/javascript"
+        src="https://d255zuevr6tr8p.cloudfront.net/build-social/main.5af497e541163275bb82.js"></script>
 </body>
 </html>

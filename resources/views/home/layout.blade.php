eb969a7f03fe1b6455924ee9f4d63fe23c7551e7
phanmduong <phanminhduong@hotmail.com>
2018-07-01 15:53:20 +0700

    UPDATE build social

--- resources/views/home/layout.blade.php
+++ resources/views/home/layout.blade.php
@@ -351,7 +351,7 @@
 </style>
 <div class="atomuser hide" id="atomuser">
     <div class="atomuser-iframe">
-        <iframe id="atomuser-iframe" src="http://localhost:3000" frameBorder="0">
+        <iframe id="atomuser-iframe" src="https://k.atomuser.com" frameBorder="0">
         </iframe>
     </div>
     <div class="atomuser-fab" id="atomuser-btn-fab">

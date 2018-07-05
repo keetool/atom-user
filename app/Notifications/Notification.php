c7f4fdffc94520b968b23cb3750836819d6392c8
Cao Anh Quan <aquancva@gmail.com>
2018-07-05 15:58:41 +0700

    add upvote and downvote notification

--- app/Notifications/Notification.php
+++ app/Notifications/Notification.php
@@ -4,35 +4,55 @@ namespace App\Notifications;
 
 abstract class Notification
 {
-    protected $title;
-    protected $detail;
-    protected $image_url;
-    protected $action_app;
-    protected $action_web;
-    protected $status;
+    public $image_url;
+    public $action_app;
+    public $action_web;
+    public $status;
+    public $type;
+    public $receiver;
+    public $actor;
 
+    public const UNSEEN = "unseen";
+    public const SEEN = "seen";
+    public const CLICKED = "clicked";
 
-    public function __construct($title, $detail, $image_url, $action_app, $action_web, $status)
+    /**
+     * Notification constructor.
+     * @param $image_url
+     * @param $action_app
+     * @param $action_web
+     * @param $status
+     * @param $type
+     * @param $receiver_id
+     * @param $author_id
+     */
+    public function __construct($image_url, $action_app, $action_web, $status, $type, $receiver, $actor)
     {
-        $this->title = $title;
-        $this->detail = $detail;
         $this->image_url = $image_url;
         $this->action_app = $action_app;
         $this->action_web = $action_web;
         $this->status = $status;
+        $this->type = $type;
+        $this->receiver = $receiver;
+        $this->actor = $actor;
     }
 
-    public static function createNotification($notification) {
+
+    public static function saveNotification(Notification $notification)
+    {
         $newNotification = new \App\Notification();
-        $newNotification->detail = $notification->detail;
-        $newNotification->action_app = $notification->detail;
-        $newNotification->action_web = $notification->detail;
-        $newNotification->status = $notification->detail;
-        $newNotification->title = $notification->detail;
-        $newNotification->image_url = $notification->detail;
+        $newNotification->detail = $notification->format();
+        $newNotification->image_url = $notification->image_url;
 
+        $newNotification->action_app = $notification->action_app;
+        $newNotification->action_web = $notification->action_web;
+
+        $newNotification->status = $notification->status;
+        $newNotification->type = $notification->type;
+        $newNotification->receiver_id = $notification->receiver->id;
+        $newNotification->actor_id = $notification->actor->id;
+        $newNotification->save();
     }
 
-
-    abstract protected function format();
+    abstract public function format();
 }

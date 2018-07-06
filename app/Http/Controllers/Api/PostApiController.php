86a2cc0aa8d307ba542778a882bdd2f2c321f587
datvithanh98 <datvithanh98@gmail.com>
2018-07-06 16:04:16 +0700

    merge

--- app/Http/Controllers/Api/PostApiController.php
+++ app/Http/Controllers/Api/PostApiController.php
@@ -139,6 +139,7 @@ class PostApiController extends ApiController
     /**
      * Post list
      * $stype = {top}
+     * param = {post_id}
      */
     public function postList($subdomain, $type, Request $request)
     {

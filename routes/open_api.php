41f25787a553c69da4016f678d4d7fc3c94b33ba
datvithanh98 <datvithanh98@gmail.com>
2018-07-09 21:00:00 +0700

    ADD load comment, post by id open api

--- routes/open_api.php
+++ routes/open_api.php
@@ -1,3 +1,8 @@
 <?php
 
 Route::get("load-post", "PostApiController@loadPosts");    
+
+Route::prefix("post")->group(function () {
+    Route::get("/{postId}", "PostApiController@getPost");
+    Route::get("/{postId}/load-comment", "CommentApiController@loadComments");
+});

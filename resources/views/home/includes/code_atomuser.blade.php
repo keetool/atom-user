310886e61dc37e7b6c3d5a0fac815ce84a1e628a
Nora <minhnora98@gmail.com>
2018-07-10 16:11:15 +0700

    ADD button copy code

---
+++ resources/views/home/includes/code_atomuser.blade.php
@@ -0,0 +1,14 @@
+<pre id="atomuser-code">
+    &lt;div class=&quot;atomuser hide&quot; id=&quot;atomuser&quot;&gt;
+        &lt;div class=&quot;atomuser-iframe&quot;&gt;
+        &lt;iframe id=&quot;atomuser-iframe&quot; src=&quot;https://@{{sub_domain}}.atomuser.com&quot; frameBorder=&quot;0&quot;&gt;
+            &lt;/iframe&gt;
+        &lt;/div&gt;
+        &lt;div class=&quot;atomuser-fab&quot; id=&quot;atomuser-btn-fab&quot;&gt;
+            &lt;div class=&quot;atomuser-icon&quot;&gt;
+                &lt;div class=&quot;atomuser-icon-dot&quot;&gt;
+                &lt;/div&gt;
+            &lt;/div&gt;
+        &lt;/div&gt;
+    &lt;/div&gt;
+</pre>

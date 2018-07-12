f90e6c16bbff014b398cdd00ad373b91ca3080af
Seenastic <goldenrezgun@gmail.com>
2018-07-12 16:44:29 +0700

    UPDATE_NotiApiCOntroller

--- routes/client_api.php
+++ routes/client_api.php
@@ -15,9 +15,9 @@ Route::middleware("auth:api")->group(function () {
     Route::prefix("user")->group(function () {
         Route::get("/", "ClientApi\UserApiController@user");
         Route::prefix("/notification")->group(function () {
+            Route::put("/{notificationId}/seen", "ClientApi\NotificationApiController@seenNotification");
             Route::get("/{notificationId}/after", "ClientApi\NotificationApiController@getNotificationsAfter");
             Route::get("/", "ClientApi\NotificationApiController@getNotifications");
-            Route::put("/{notificationId}/seen", "ClientApi\NotificationApiController@seenNotification");
         });
 
         Route::get("/list/{type}", "ClientApi\UserApiController@userList");

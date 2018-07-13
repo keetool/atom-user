269bda6270552a3fbaa0e7fec353cdc154fa3dd1
datvithanh98 <datvithanh98@gmail.com>
2018-07-13 10:44:44 +0700

    MOD search api

--- app/Http/Controllers/ClientApi/ClientApiController.php
+++ app/Http/Controllers/ClientApi/ClientApiController.php
@@ -18,6 +18,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\Post as PostResource;
+use App\Services\AppService;
 
 /**
  * @resource Client
@@ -30,13 +31,15 @@ class ClientApiController extends ApiController
     protected $merchantRepo;
     protected $voteRepo;
     protected $socketService;
+    protected $appService;
 
     public function __construct(
         VoteRepositoryInterface $voteRepository,
         PostRepositoryInterface $postRepo,
         SocketServiceInterface $socketService,
         ImagePostRepositoryInterface $imagePostRepository,
-        MerchantRepository $merchantRepository
+        MerchantRepository $merchantRepository,
+        AppService $appService
     ) {
         parent::__construct();
         $this->postRepo = $postRepo;
@@ -44,6 +47,7 @@ class ClientApiController extends ApiController
         $this->voteRepo = $voteRepository;
         $this->socketService = $socketService;
         $this->imagePostRepository = $imagePostRepository;
+        $this->appService = $appService;
     }
 
     /** 
@@ -52,9 +56,12 @@ class ClientApiController extends ApiController
      */
     public function search($subDomain, Request $request)
     {
-        $search = $request->search;
+        $search = $this->appService->convert_vi_to_en(strtolower($request->search));
+        
         $merchant = $this->merchantRepo->findBySubDomain($request->subDomain);
+        
+        $posts = $this->postRepo->searchByMerchantId($merchant->id, $search, $request->post_id);
 
-        return $this->success(["posts" => PostFullResource::collection($this->postRepo->searchByMerchantId($merchant->id, $search, $request->post_id))]);
+        return $this->success(["posts" => PostFullResource::collection($posts)]);
     }
 }
